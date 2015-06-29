0289ccf9a3b7827ac1de7c74fe88e809781a00bb
Robert Knight <robert.knight@mendeley.com>
2015-06-29 12:50:32 +0100

    Improve error reporting when exec() fails
    Reject the promise returned by exec() with an Error
    subclass that has a useful message when the command
    fails.
    
    This makes debugging an exec() failure in the middle
    of a chain of promises easier.

--- src/exec.js
+++ src/exec.js
@@ -1,23 +1,34 @@
 var child_process = require('child_process');
 var fs = require('fs');
 var Q = require('q');
+var util = require('util');
+
+function CommandError(cmd, status, stdout, stderr) {
+	Error.call(this);
+	this.message = 'Command \'' + cmd + '\' failed with status ' + status + ': ' + stderr;
+	this.status = status;
+	this.stdout = stdout;
+	this.stderr = stderr;
+}
+util.inherits(CommandError, Error);
 
 function exec() {
 	var result = Q.defer();
-	var proc = child_process.spawn(arguments[0], Array.prototype.slice.call(arguments,1));
+	var cmdArgs = Array.prototype.slice.call(arguments);
+	var proc = child_process.spawn(cmdArgs[0], cmdArgs.slice(1));
 	var stdout = '';
 	var stderr = '';
 	proc.stdout.on('data', function(data) {
 		stdout += data.toString();
 	});
 	proc.stderr.on('data', function(data) {
-		stderr += '';
+		stderr += data.toString();
 	});
 	proc.on('close', function(status) {
 		if (status == 0) {
 			result.resolve([stdout, stderr]);
 		} else {
-			result.reject([status, stdout, stderr]);
+			result.reject(new CommandError(cmdArgs.join(" "), status, stdout, stderr));
 		}
 	});
 	return result.promise;
